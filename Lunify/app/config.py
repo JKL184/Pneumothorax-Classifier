e720bf3d00dd255a211fee92b9683b94a520e1a5
JKL184 <jordankareemlewis@gmail.com>
2022-04-09 22:42:59 -0500

    changed db manager to more recent version

--- Lunify/app/config.py
+++ Lunify/app/config.py
@@ -6,7 +6,9 @@ load_dotenv()  # load environment variables from .env if it exists.
 class Config(object):
     """Base Config Object"""
     DEBUG = False
-    SECRET_KEY = "lungify"
+    #SECRET_KEY = "lungify"
     UPLOAD_FOLDER = './app/static/uploads'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://lungify:lungify@localhost/lungify_db'
+    #SQLALCHEMY_DATABASE_URI = 'postgresql://lungify:lungify@localhost/lungify_db'
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'Som3$ec5etK*y')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://')
     SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
