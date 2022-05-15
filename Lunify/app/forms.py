9ecdb89e8e51f5fa9285266dfee8716746c9b6bd
saphw <saphrahann.wint@gmail.com>
2022-05-14 19:35:08 -0500

    settings

--- Lunify/app/forms.py
+++ Lunify/app/forms.py
@@ -4,9 +4,9 @@ from wtforms import StringField, IntegerField, PasswordField
 from wtforms.validators import InputRequired, DataRequired,Email
 
 class settingsForm(FlaskForm):
-
     size= IntegerField("Small Pneumothorax Size", validators=[DataRequired()])
     email = StringField('Email', validators=[InputRequired()])
+
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[InputRequired()])
